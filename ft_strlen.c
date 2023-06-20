f6b27bbada99b2f8ab019356084132b1c60ae3aa
maariovp <maariovp@gmail.com>
2023-06-20 01:30:52 +0100

    Update ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -12,9 +12,9 @@
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
-	size_t	cont;
+	int	cont;
 
 	cont = 0;
 	while (s[cont])
--- libft.h
+++ libft.h
@@ -90,7 +90,7 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size);
 
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 
-size_t		ft_strlen(const char *s);
+int		ft_strlen(const char *s);
 
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
